b1699468aae21295a79bc820a031c3ac874ccf3b
Jack Regan <me@paulregan.co.uk>
2013-08-27 12:25:42 +0100

    adding chef files

---
+++ .chef/knife.rb
@@ -0,0 +1,9 @@
+log_level                :info
+log_location             STDOUT
+node_name                'paul'
+client_key               '/Users/paul/chef-repo/.chef/paul.pem'
+validation_client_name   'chef-validator'
+validation_key           '/Users/paul/chef-repo/.chef/chef-validator.pem'
+chef_server_url          'https://chef.prlab.com'
+syntax_check_cache_path  '/Users/paul/chef-repo/.chef/syntax_check_cache'
+cookbook_path [ '/Users/paul/chef-repo/cookbooks' ]
